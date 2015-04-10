950a601705d38a2d2d191fdbe03927b6de798dcb
Justin Brown <jkbmr2@mst.edu>
2015-04-09 23:06:31 -0500

    UI
    Made some changes to my UI pages

--- app/src/main/java/rsck/chalkboard/ClassOverView.java
+++ app/src/main/java/rsck/chalkboard/ClassOverView.java
@@ -5,19 +5,36 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
+import java.lang.reflect.Type;
+
 public class ClassOverView extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstances){
         super.onCreate(savedInstances);
         setContentView(R.layout.class_overview);
+
+
         //Change the font of text
         //The font path
-        String fontPath = "fonts/chalk_font.ttf";
+        String chalkFontPath = "fonts/chalk_font.ttf";
+        String robotoFontPath = "fonts/roboto_light.ttf";
+
+
         //Connect the text view
+        //TextView cardTitles = (TextView) findViewById(R.id.card_title_id);
         TextView currentGradeText = (TextView) findViewById(R.id.Current_grade_text);
+        TextView currentGrade = (TextView) findViewById(R.id.current_grade);
+
+
         //Make the new typeface (font)
-        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        Typeface tf = Typeface.createFromAsset(getAssets(), chalkFontPath);
+        //Typeface rl = Typeface.createFromAsset(getAssets(), robotoFontPath);
+
         //Set the new typeface (font)
         currentGradeText.setTypeface(tf);
+        currentGrade.setTypeface(tf);
+        //cardTitles.setTypeface(rl);
+
+
     }
 }
