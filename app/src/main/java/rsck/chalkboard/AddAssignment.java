8c820a1a095f0f530c0e9a166daacc7ba15ea060
Justin Brown <jkbmr2@mst.edu>
2015-04-22 18:08:05 -0500

    Sign Up Button
    Added the sign up button

---
+++ app/src/main/java/rsck/chalkboard/AddAssignment.java
@@ -0,0 +1,15 @@
+package rsck.chalkboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddAssignment extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_assignment);
+
+
+    }
+
+}
