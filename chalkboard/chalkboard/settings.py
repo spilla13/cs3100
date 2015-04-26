18f09b6480b887279fdb7e389abd1921b4af26b7
Billy Rhoades <billyarhoades@gmail.com>
2015-04-26 17:26:11 -0500

    Changing to postgresql

--- chalkboard/chalkboard/settings.py
+++ chalkboard/chalkboard/settings.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'chalkboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'chalkboard'
     }
 }
 
